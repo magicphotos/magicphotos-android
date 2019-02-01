8eac9069bb518f8f44885d58603f719f4fe12b4b
Oleg Derevenetz <oleg.derevenetz@gmail.com>
2019-02-01 18:31:38 +0300

    Update scripts for Qt 5.12.1, separate .ts for src and qml parts.

--- tools/gen_ts.bat
+++ tools/gen_ts.bat
@@ -1,3 +1,14 @@
-PATH=%PATH%;C:\Qt\5.12.0\mingw73_64\bin;C:\Qt\Tools\mingw730_64\bin
+PATH=%PATH%;C:\Qt\5.12.1\mingw73_64\bin;C:\Qt\Tools\mingw730_64\bin
 
-lupdate ..\magicphotos.pro
+lupdate ..\magicphotos.pro -ts ..\translations\magicphotos_ru.src.ts
+lupdate ..\qml             -ts ..\translations\magicphotos_ru.qml.ts
+
+lupdate ..\magicphotos.pro -ts ..\translations\magicphotos_de.src.ts
+lupdate ..\qml             -ts ..\translations\magicphotos_de.qml.ts
+
+lupdate ..\magicphotos.pro -ts ..\translations\magicphotos_fr.src.ts
+lupdate ..\qml             -ts ..\translations\magicphotos_fr.qml.ts
+
+lconvert ..\translations\magicphotos_ru.src.ts ..\translations\magicphotos_ru.qml.ts -o ..\translations\magicphotos_ru.ts
+lconvert ..\translations\magicphotos_de.src.ts ..\translations\magicphotos_de.qml.ts -o ..\translations\magicphotos_de.ts
+lconvert ..\translations\magicphotos_fr.src.ts ..\translations\magicphotos_fr.qml.ts -o ..\translations\magicphotos_fr.ts
