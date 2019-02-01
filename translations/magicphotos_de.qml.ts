4056809e3c3ef6b487589f0e83c6e567da0fad95
Oleg Derevenetz <oleg.derevenetz@gmail.com>
2019-02-01 18:33:23 +0300

    Update translations.

---
+++ translations/magicphotos_de.qml.ts
@@ -0,0 +1,391 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>BlurPage</name>
+    <message>
+        <source>https://magicphotos.sourceforge.io/help/android/help.html</source>
+        <translation>https://magicphotos.sourceforge.io/help/android/help_de.html</translation>
+    </message>
+    <message>
+        <source>Brush Size</source>
+        <translation>Pinselgröße</translation>
+    </message>
+    <message>
+        <source>Brush Opacity</source>
+        <translation>Pinsel Härte</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>Fehler</translation>
+    </message>
+    <message>
+        <source>Could not open image</source>
+        <translation>Bild kann nicht geöffnet werden</translation>
+    </message>
+    <message>
+        <source>Info</source>
+        <translation>Information</translation>
+    </message>
+    <message>
+        <source>Image saved successfully</source>
+        <translation>Abbildung wurde erfolgreich gespeichert</translation>
+    </message>
+    <message>
+        <source>Could not save image</source>
+        <translation>Abbildung kann nicht gespeichert werden</translation>
+    </message>
+    <message>
+        <source>Warning</source>
+        <translation>Achtung</translation>
+    </message>
+    <message>
+        <source>Are you sure? Current image is not saved and will be lost.</source>
+        <translation>Sind Sie sicher? Alle nicht gespeicherten Anderungen gehen verloren.</translation>
+    </message>
+</context>
+<context>
+    <name>BlurPreviewPage</name>
+    <message>
+        <source>Blur</source>
+        <translation>Verwischen</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>Fehler</translation>
+    </message>
+    <message>
+        <source>Could not open image</source>
+        <translation>Bild kann nicht geöffnet werden</translation>
+    </message>
+</context>
+<context>
+    <name>CartoonPage</name>
+    <message>
+        <source>https://magicphotos.sourceforge.io/help/android/help.html</source>
+        <translation>https://magicphotos.sourceforge.io/help/android/help_de.html</translation>
+    </message>
+    <message>
+        <source>Brush Size</source>
+        <translation>Pinselgröße</translation>
+    </message>
+    <message>
+        <source>Brush Opacity</source>
+        <translation>Pinsel Härte</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>Fehler</translation>
+    </message>
+    <message>
+        <source>Could not open image</source>
+        <translation>Bild kann nicht geöffnet werden</translation>
+    </message>
+    <message>
+        <source>Info</source>
+        <translation>Information</translation>
+    </message>
+    <message>
+        <source>Image saved successfully</source>
+        <translation>Abbildung wurde erfolgreich gespeichert</translation>
+    </message>
+    <message>
+        <source>Could not save image</source>
+        <translation>Abbildung kann nicht gespeichert werden</translation>
+    </message>
+    <message>
+        <source>Warning</source>
+        <translation>Achtung</translation>
+    </message>
+    <message>
+        <source>Are you sure? Current image is not saved and will be lost.</source>
+        <translation>Sind Sie sicher? Alle nicht gespeicherten Anderungen gehen verloren.</translation>
+    </message>
+</context>
+<context>
+    <name>CartoonPreviewPage</name>
+    <message>
+        <source>Cartoon</source>
+        <translation>In den Comic Strip</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>Fehler</translation>
+    </message>
+    <message>
+        <source>Could not open image</source>
+        <translation>Bild kann nicht geöffnet werden</translation>
+    </message>
+</context>
+<context>
+    <name>DecolorizePage</name>
+    <message>
+        <source>https://magicphotos.sourceforge.io/help/android/help.html</source>
+        <translation>https://magicphotos.sourceforge.io/help/android/help_de.html</translation>
+    </message>
+    <message>
+        <source>Brush Size</source>
+        <translation>Pinselgröße</translation>
+    </message>
+    <message>
+        <source>Brush Opacity</source>
+        <translation>Pinsel Härte</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>Fehler</translation>
+    </message>
+    <message>
+        <source>Could not open image</source>
+        <translation>Bild kann nicht geöffnet werden</translation>
+    </message>
+    <message>
+        <source>Info</source>
+        <translation>Information</translation>
+    </message>
+    <message>
+        <source>Image saved successfully</source>
+        <translation>Abbildung wurde erfolgreich gespeichert</translation>
+    </message>
+    <message>
+        <source>Could not save image</source>
+        <translation>Abbildung kann nicht gespeichert werden</translation>
+    </message>
+    <message>
+        <source>Warning</source>
+        <translation>Achtung</translation>
+    </message>
+    <message>
+        <source>Are you sure? Current image is not saved and will be lost.</source>
+        <translation>Sind Sie sicher? Alle nicht gespeicherten Anderungen gehen verloren.</translation>
+    </message>
+</context>
+<context>
+    <name>ModeSelectionPage</name>
+    <message>
+        <source>MagicPhotos</source>
+        <translation>MagicPhotos</translation>
+    </message>
+    <message>
+        <source>https://magicphotos.sourceforge.io/help/android/help.html</source>
+        <translation>https://magicphotos.sourceforge.io/help/android/help_de.html</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>Fehler</translation>
+    </message>
+    <message>
+        <source>Could not open image</source>
+        <translation>Bild kann nicht geöffnet werden</translation>
+    </message>
+</context>
+<context>
+    <name>PixelatePage</name>
+    <message>
+        <source>https://magicphotos.sourceforge.io/help/android/help.html</source>
+        <translation>https://magicphotos.sourceforge.io/help/android/help_de.html</translation>
+    </message>
+    <message>
+        <source>Brush Size</source>
+        <translation>Pinselgröße</translation>
+    </message>
+    <message>
+        <source>Brush Opacity</source>
+        <translation>Pinsel Härte</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>Fehler</translation>
+    </message>
+    <message>
+        <source>Could not open image</source>
+        <translation>Bild kann nicht geöffnet werden</translation>
+    </message>
+    <message>
+        <source>Info</source>
+        <translation>Information</translation>
+    </message>
+    <message>
+        <source>Image saved successfully</source>
+        <translation>Abbildung wurde erfolgreich gespeichert</translation>
+    </message>
+    <message>
+        <source>Could not save image</source>
+        <translation>Abbildung kann nicht gespeichert werden</translation>
+    </message>
+    <message>
+        <source>Warning</source>
+        <translation>Achtung</translation>
+    </message>
+    <message>
+        <source>Are you sure? Current image is not saved and will be lost.</source>
+        <translation>Sind Sie sicher? Alle nicht gespeicherten Anderungen gehen verloren.</translation>
+    </message>
+</context>
+<context>
+    <name>PixelatePreviewPage</name>
+    <message>
+        <source>Pixelate</source>
+        <translation>Pixeln</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>Fehler</translation>
+    </message>
+    <message>
+        <source>Could not open image</source>
+        <translation>Bild kann nicht geöffnet werden</translation>
+    </message>
+</context>
+<context>
+    <name>RecolorPage</name>
+    <message>
+        <source>https://magicphotos.sourceforge.io/help/android/help.html</source>
+        <translation>https://magicphotos.sourceforge.io/help/android/help_de.html</translation>
+    </message>
+    <message>
+        <source>Brush Size</source>
+        <translation>Pinselgröße</translation>
+    </message>
+    <message>
+        <source>Brush Opacity</source>
+        <translation>Pinsel Härte</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>Fehler</translation>
+    </message>
+    <message>
+        <source>Could not open image</source>
+        <translation>Bild kann nicht geöffnet werden</translation>
+    </message>
+    <message>
+        <source>Info</source>
+        <translation>Information</translation>
+    </message>
+    <message>
+        <source>Image saved successfully</source>
+        <translation>Abbildung wurde erfolgreich gespeichert</translation>
+    </message>
+    <message>
+        <source>Could not save image</source>
+        <translation>Abbildung kann nicht gespeichert werden</translation>
+    </message>
+    <message>
+        <source>Warning</source>
+        <translation>Achtung</translation>
+    </message>
+    <message>
+        <source>Are you sure? Current image is not saved and will be lost.</source>
+        <translation>Sind Sie sicher? Alle nicht gespeicherten Anderungen gehen verloren.</translation>
+    </message>
+</context>
+<context>
+    <name>RetouchPage</name>
+    <message>
+        <source>https://magicphotos.sourceforge.io/help/android/help.html</source>
+        <translation>https://magicphotos.sourceforge.io/help/android/help_de.html</translation>
+    </message>
+    <message>
+        <source>Brush Size</source>
+        <translation>Pinselgröße</translation>
+    </message>
+    <message>
+        <source>Brush Opacity</source>
+        <translation>Pinsel Härte</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>Fehler</translation>
+    </message>
+    <message>
+        <source>Could not open image</source>
+        <translation>Bild kann nicht geöffnet werden</translation>
+    </message>
+    <message>
+        <source>Info</source>
+        <translation>Information</translation>
+    </message>
+    <message>
+        <source>Image saved successfully</source>
+        <translation>Abbildung wurde erfolgreich gespeichert</translation>
+    </message>
+    <message>
+        <source>Could not save image</source>
+        <translation>Abbildung kann nicht gespeichert werden</translation>
+    </message>
+    <message>
+        <source>Warning</source>
+        <translation>Achtung</translation>
+    </message>
+    <message>
+        <source>Are you sure? Current image is not saved and will be lost.</source>
+        <translation>Sind Sie sicher? Alle nicht gespeicherten Anderungen gehen verloren.</translation>
+    </message>
+</context>
+<context>
+    <name>SketchPage</name>
+    <message>
+        <source>https://magicphotos.sourceforge.io/help/android/help.html</source>
+        <translation>https://magicphotos.sourceforge.io/help/android/help_de.html</translation>
+    </message>
+    <message>
+        <source>Brush Size</source>
+        <translation>Pinselgröße</translation>
+    </message>
+    <message>
+        <source>Brush Opacity</source>
+        <translation>Pinsel Härte</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>Fehler</translation>
+    </message>
+    <message>
+        <source>Could not open image</source>
+        <translation>Bild kann nicht geöffnet werden</translation>
+    </message>
+    <message>
+        <source>Info</source>
+        <translation>Information</translation>
+    </message>
+    <message>
+        <source>Image saved successfully</source>
+        <translation>Abbildung wurde erfolgreich gespeichert</translation>
+    </message>
+    <message>
+        <source>Could not save image</source>
+        <translation>Abbildung kann nicht gespeichert werden</translation>
+    </message>
+    <message>
+        <source>Warning</source>
+        <translation>Achtung</translation>
+    </message>
+    <message>
+        <source>Are you sure? Current image is not saved and will be lost.</source>
+        <translation>Sind Sie sicher? Alle nicht gespeicherten Anderungen gehen verloren.</translation>
+    </message>
+</context>
+<context>
+    <name>SketchPreviewPage</name>
+    <message>
+        <source>Sketch</source>
+        <translation>In die skizze</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>Fehler</translation>
+    </message>
+    <message>
+        <source>Could not open image</source>
+        <translation>Bild kann nicht geöffnet werden</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <source>MagicPhotos</source>
+        <translation>MagicPhotos</translation>
+    </message>
+</context>
+</TS>
