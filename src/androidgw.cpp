9a10bc3bc5498bba21973560d6b06db8cb12a07a
Oleg Derevenetz <oleg.derevenetz@gmail.com>
2019-03-15 16:59:07 +0300

    Use macro to form names for Java native methods.

--- src/androidgw.cpp
+++ src/androidgw.cpp
@@ -2,6 +2,8 @@
 
 #include "androidgw.h"
 
+#define JAVA_NATIVE_METHOD_NAME(class_name, method_name) Java_com_derevenetz_oleg_magicphotos_stdalone_ ## class_name ## _ ## method_name
+
 AndroidGW *AndroidGW::Instance = nullptr;
 
 AndroidGW::AndroidGW(QObject *parent) : QObject(parent)
@@ -14,12 +16,12 @@ AndroidGW *AndroidGW::instance()
     return Instance;
 }
 
-extern "C" JNIEXPORT void JNICALL Java_com_derevenetz_oleg_magicphotos_stdalone_MagicActivity_bannerViewHeightUpdated(JNIEnv *, jclass, jint height)
+extern "C" JNIEXPORT void JNICALL JAVA_NATIVE_METHOD_NAME(MagicActivity, bannerViewHeightUpdated)(JNIEnv *, jclass, jint height)
 {
     emit AndroidGW::instance()->setBannerViewHeight(height);
 }
 
-extern "C" JNIEXPORT void JNICALL Java_com_derevenetz_oleg_magicphotos_stdalone_MagicActivity_imageSelected(JNIEnv *jni_env, jclass, jstring j_image_file, jint image_orientation)
+extern "C" JNIEXPORT void JNICALL JAVA_NATIVE_METHOD_NAME(MagicActivity, imageSelected)(JNIEnv *jni_env, jclass, jstring j_image_file, jint image_orientation)
 {
     const char* str        = jni_env->GetStringUTFChars(j_image_file, nullptr);
     QString     image_file = str;
@@ -29,12 +31,12 @@ extern "C" JNIEXPORT void JNICALL Java_com_derevenetz_oleg_magicphotos_stdalone_
     emit AndroidGW::instance()->processImageSelection(image_file, image_orientation);
 }
 
-extern "C" JNIEXPORT void JNICALL Java_com_derevenetz_oleg_magicphotos_stdalone_MagicActivity_imageSelectionCancelled(JNIEnv *)
+extern "C" JNIEXPORT void JNICALL JAVA_NATIVE_METHOD_NAME(MagicActivity, imageSelectionCancelled)(JNIEnv *)
 {
     emit AndroidGW::instance()->processImageSelectionCancel();
 }
 
-extern "C" JNIEXPORT void JNICALL Java_com_derevenetz_oleg_magicphotos_stdalone_MagicActivity_imageSelectionFailed(JNIEnv *)
+extern "C" JNIEXPORT void JNICALL JAVA_NATIVE_METHOD_NAME(MagicActivity, imageSelectionFailed)(JNIEnv *)
 {
     emit AndroidGW::instance()->processImageSelectionFailure();
 }
