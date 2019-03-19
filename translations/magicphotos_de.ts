584e45174cd65411fd31e7833fb100612084fb28
Oleg Derevenetz <oleg.derevenetz@gmail.com>
2019-03-19 21:40:05 +0300

    Add extra Qt localization for MessageDialog OK/Yes/No buttons.

--- translations/magicphotos_de.ts
+++ translations/magicphotos_de.ts
@@ -388,4 +388,30 @@
         <translation>MagicPhotos</translation>
     </message>
 </context>
+<context>
+    <name>QPlatformTheme</name>
+    <message>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <source>&amp;Yes</source>
+        <translation>&amp;Ja</translation>
+    </message>
+    <message>
+        <source>&amp;No</source>
+        <translation>&amp;Nein</translation>
+    </message>
+</context>
+<context>
+    <name>QAndroidPlatformTheme</name>
+    <message>
+        <source>Yes</source>
+        <translation>Ja</translation>
+    </message>
+    <message>
+        <source>No</source>
+        <translation>Nein</translation>
+    </message>
+</context>
 </TS>
--- translations/magicphotos_fr.ts
+++ translations/magicphotos_fr.ts
@@ -388,4 +388,30 @@
         <translation>MagicPhotos</translation>
     </message>
 </context>
+<context>
+    <name>QPlatformTheme</name>
+    <message>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <source>&amp;Yes</source>
+        <translation>&amp;Oui</translation>
+    </message>
+    <message>
+        <source>&amp;No</source>
+        <translation>&amp;Non</translation>
+    </message>
+</context>
+<context>
+    <name>QAndroidPlatformTheme</name>
+    <message>
+        <source>Yes</source>
+        <translation>Oui</translation>
+    </message>
+    <message>
+        <source>No</source>
+        <translation>Non</translation>
+    </message>
+</context>
 </TS>
