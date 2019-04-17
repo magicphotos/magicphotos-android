e903767a23b23ab44e1b644cdb71306378f6627e
Oleg Derevenetz <oleg.derevenetz@gmail.com>
2019-04-18 00:24:53 +0300

    Change name of SonarCloud build directory to ".sonarbuild", make it hidden, do not try (and fail) to recreate it if it is already exists, style nits.

--- tools/sonarcloud.bat
+++ tools/sonarcloud.bat
@@ -5,22 +5,22 @@ SET SONARCLOUD_ROOT=D:\Users\%USERNAME%\Documents\SonarCloud
 
 PATH=%PATH%;C:\Qt\5.12.2\android_armv7\bin;%ANDROID_NDK_ROOT%\prebuilt\windows\bin;%SONARCLOUD_ROOT%\build-wrapper-win-x86;%SONARCLOUD_ROOT%\sonar-scanner-3.3.0.1492-windows\bin
 
-IF "%~1"=="" (
+IF NOT "%~1"=="" (
+        CD .. ^
+     && (IF NOT EXIST .sonarbuild (MKDIR .sonarbuild && ATTRIB +H .sonarbuild) || CD .) ^
+     && CD .sonarbuild ^
+     && qmake.exe ..\magicphotos.pro ^
+     && build-wrapper-win-x86-64.exe --out-dir bw-output make.exe clean all ^
+     && CD .. ^
+     && sonar-scanner.bat -D"sonar.projectKey=oleg-derevenetz:magicphotos-android" ^
+                          -D"sonar.projectName=MagicPhotos Android" ^
+                          -D"sonar.projectVersion=master" ^
+                          -D"sonar.organization=oleg-derevenetz-github" ^
+                          -D"sonar.sources=src" ^
+                          -D"sonar.sourceEncoding=UTF-8" ^
+                          -D"sonar.cfamily.build-wrapper-output=.sonarbuild\\bw-output" ^
+                          -D"sonar.host.url=https://sonarcloud.io" ^
+                          -D"sonar.login=%~1"
+) ELSE (
     ECHO Syntax: sonarcloud.bat SONARCLOUD_KEY
-    EXIT
 )
-
-CD .. && ^
-MKDIR __BUILD__ && CD __BUILD__ && ^
-qmake ..\magicphotos.pro && ^
-build-wrapper-win-x86-64.exe --out-dir bw-output make.exe clean all && ^
-CD .. && ^
-sonar-scanner.bat -D"sonar.projectKey=oleg-derevenetz:magicphotos-android" ^
-                  -D"sonar.projectName=MagicPhotos Android" ^
-                  -D"sonar.projectVersion=master" ^
-                  -D"sonar.organization=oleg-derevenetz-github" ^
-                  -D"sonar.sources=src" ^
-                  -D"sonar.sourceEncoding=UTF-8" ^
-                  -D"sonar.cfamily.build-wrapper-output=__BUILD__\\bw-output" ^
-                  -D"sonar.host.url=https://sonarcloud.io" ^
-                  -D"sonar.login=%~1"
