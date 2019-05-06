65f28d658ed422bbcfdc50203f05663c8b7bf16d
Oleg Derevenetz <oleg.derevenetz@gmail.com>
2019-05-06 04:56:43 +0300

    Make full build (with Java) for SonarCloud.

--- tools/sonarcloud.bat
+++ tools/sonarcloud.bat
@@ -1,7 +1,10 @@
 @ECHO OFF
 
+SET JAVA_HOME=C:\Program Files (x86)\Java\jdk1.8.0_202
+SET ANDROID_SDK_ROOT=D:\Users\%USERNAME%\Documents\Android\android-sdk
 SET ANDROID_NDK_ROOT=D:\Users\%USERNAME%\Documents\Android\android-ndk-r18b
 SET SONARCLOUD_ROOT=D:\Users\%USERNAME%\Documents\SonarCloud
+SET ANDROID_PLATFORM=android-27
 
 PATH=%PATH%;C:\Qt\5.12.2\android_armv7\bin;%ANDROID_NDK_ROOT%\prebuilt\windows\bin;%SONARCLOUD_ROOT%\build-wrapper-win-x86;%SONARCLOUD_ROOT%\sonar-scanner-3.3.0.1492-windows\bin
 
@@ -10,7 +13,8 @@ IF NOT "%~1"=="" (
      && (IF NOT EXIST .sonarbuild (MKDIR .sonarbuild && ATTRIB +H .sonarbuild) || CD .) ^
      && CD .sonarbuild ^
      && qmake.exe ..\magicphotos.pro ^
-     && build-wrapper-win-x86-64.exe --out-dir bw-output make.exe clean all ^
+     && build-wrapper-win-x86-64.exe --out-dir bw-output make.exe clean all install INSTALL_ROOT=android-output ^
+     && androiddeployqt.exe --input android-libmagicphotos.so-deployment-settings.json --output android-output --android-platform %ANDROID_PLATFORM% --deployment bundled --gradle --no-gdbserver ^
      && CD .. ^
      && sonar-scanner.bat -D"sonar.projectKey=magicphotos:magicphotos-android" ^
                           -D"sonar.projectName=MagicPhotos Android" ^
@@ -20,6 +24,7 @@ IF NOT "%~1"=="" (
                           -D"sonar.exclusions=qml/**/*,translations/*" ^
                           -D"sonar.cfamily.build-wrapper-output=.sonarbuild\\bw-output" ^
                           -D"sonar.java.source=1.7" ^
+                          -D"sonar.java.binaries=.sonarbuild" ^
                           -D"sonar.host.url=https://sonarcloud.io" ^
                           -D"sonar.login=%~1"
 ) ELSE (
