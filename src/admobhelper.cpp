860260d4349d889098d741439001b3e0ad2c3d9f
Oleg Derevenetz <oleg.derevenetz@gmail.com>
2019-08-22 22:24:45 +0300

    Remove unused ADMOB_APP_ID, app id is now set via AndroidManifest.xml.

--- src/admobhelper.cpp
+++ src/admobhelper.cpp
@@ -3,7 +3,6 @@
 
 #include "admobhelper.h"
 
-const QString AdMobHelper::ADMOB_APP_ID              ("ca-app-pub-2455088855015693~7279538773");
 const QString AdMobHelper::ADMOB_BANNERVIEW_UNIT_ID  ("ca-app-pub-2455088855015693/1811713388");
 const QString AdMobHelper::ADMOB_INTERSTITIAL_UNIT_ID("ca-app-pub-2455088855015693/6662028461");
 
@@ -28,11 +27,9 @@ int AdMobHelper::bannerViewHeight() const
 void AdMobHelper::initAds()
 {
     if (!Initialized) {
-        QAndroidJniObject j_app_id               = QAndroidJniObject::fromString(ADMOB_APP_ID);
         QAndroidJniObject j_interstitial_unit_id = QAndroidJniObject::fromString(ADMOB_INTERSTITIAL_UNIT_ID);
 
-        QtAndroid::androidActivity().callMethod<void>("initAds", "(Ljava/lang/String;Ljava/lang/String;)V", j_app_id.object<jstring>(),
-                                                                                                            j_interstitial_unit_id.object<jstring>());
+        QtAndroid::androidActivity().callMethod<void>("initAds", "(Ljava/lang/String;)V", j_interstitial_unit_id.object<jstring>());
 
         Initialized = true;
     }
