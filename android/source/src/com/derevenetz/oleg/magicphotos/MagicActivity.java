c54901c33c4fb082dd9193ab895dab1bfb0a021b
Oleg Derevenetz <oleg.derevenetz@gmail.com>
2019-10-11 14:32:46 +0300

    Tag ad requests as child-directed to participate in Google Play's "Designed for Families" program.
    This reverts commit f90d1211907bf5b4058c9000dcca8821154438f5.

--- android/source/src/com/derevenetz/oleg/magicphotos/MagicActivity.java
+++ android/source/src/com/derevenetz/oleg/magicphotos/MagicActivity.java
@@ -212,6 +212,7 @@ public class MagicActivity extends QtActivity
             {
                 MobileAds.setRequestConfiguration(MobileAds.getRequestConfiguration()
                                                            .toBuilder().setMaxAdContentRating(RequestConfiguration.MAX_AD_CONTENT_RATING_G)
+                                                                       .setTagForChildDirectedTreatment(RequestConfiguration.TAG_FOR_CHILD_DIRECTED_TREATMENT_TRUE)
                                                                        .build());
 
                 MobileAds.initialize(f_context);
