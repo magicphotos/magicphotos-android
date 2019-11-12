ea6233008120d040994bbf8eb0736c1e339a9b86
Oleg Derevenetz <oleg.derevenetz@gmail.com>
2019-11-12 03:09:01 +0300

    Style nits: sort files in project and scripts alphabetically.

--- tools/gen_ts.bat
+++ tools/gen_ts.bat
@@ -1,11 +1,11 @@
 PATH=%PATH%;C:\Qt\5.12.4\mingw73_64\bin;C:\Qt\Tools\mingw730_64\bin
 
-lupdate -locations absolute ..\magicphotos.pro -ts ..\translations\magicphotos_ru.src.ts
-
 lupdate -locations absolute ..\magicphotos.pro -ts ..\translations\magicphotos_de.src.ts
 
 lupdate -locations absolute ..\magicphotos.pro -ts ..\translations\magicphotos_fr.src.ts
 
-lconvert ..\translations\magicphotos_ru.src.ts ..\translations\magicphotos_ru.qt.ts -o ..\translations\magicphotos_ru.ts
+lupdate -locations absolute ..\magicphotos.pro -ts ..\translations\magicphotos_ru.src.ts
+
 lconvert ..\translations\magicphotos_de.src.ts ..\translations\magicphotos_de.qt.ts -o ..\translations\magicphotos_de.ts
 lconvert ..\translations\magicphotos_fr.src.ts ..\translations\magicphotos_fr.qt.ts -o ..\translations\magicphotos_fr.ts
+lconvert ..\translations\magicphotos_ru.src.ts ..\translations\magicphotos_ru.qt.ts -o ..\translations\magicphotos_ru.ts
