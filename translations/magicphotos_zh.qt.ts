93a6a13d0fa28b30a2a81d6f345ae763610d6e6c
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2020-05-31 00:12:02 +0300

    Add Chinese translation.

---
+++ translations/magicphotos_zh.qt.ts
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>QPlatformTheme</name>
+    <message>
+        <source>OK</source>
+        <translation>好</translation>
+    </message>
+    <message>
+        <source>&amp;Yes</source>
+        <translation>&amp;是</translation>
+    </message>
+    <message>
+        <source>&amp;No</source>
+        <translation>&amp;没有</translation>
+    </message>
+</context>
+<context>
+    <name>QAndroidPlatformTheme</name>
+    <message>
+        <source>Yes</source>
+        <translation>是</translation>
+    </message>
+    <message>
+        <source>No</source>
+        <translation>没有</translation>
+    </message>
+</context>
+</TS>
