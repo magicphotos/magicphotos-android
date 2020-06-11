17520a350349e82c1db94a1468c8f61161258acb
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2020-06-12 00:15:24 +0300

    Update scripts: use "SET PATH=" instead of "PATH=", use newer JDK.

--- tools/gradlew_lint.bat
+++ tools/gradlew_lint.bat
@@ -1,12 +1,12 @@
 @ECHO OFF
 
-SET JAVA_HOME=C:\Program Files (x86)\Java\jdk1.8.0_202
+SET JAVA_HOME=C:\Program Files\Java\jdk1.8.0_251
 SET ANDROID_SDK_ROOT=%USERPROFILE%\AppData\Local\Android\Sdk
 SET ANDROID_NDK_ROOT=%USERPROFILE%\AppData\Local\Android\Sdk\ndk\20.0.5594570
 SET ANDROID_NDK_HOST=windows
 SET ANDROID_PLATFORM=android-29
 
-PATH=%PATH%;C:\Qt\5.12.8\android_armv7\bin;%ANDROID_NDK_ROOT%\prebuilt\windows\bin
+SET PATH=%PATH%;C:\Qt\5.12.8\android_armv7\bin;%ANDROID_NDK_ROOT%\prebuilt\windows\bin
 
     CD .. ^
  && (IF NOT EXIST .gradlewbuild (MKDIR .gradlewbuild && ATTRIB +H .gradlewbuild)) ^
