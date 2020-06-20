d9a777c092ec5e062acf109ece0b72753949535c
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2020-06-20 12:50:25 +0300

    Use NDK 21.0.6113669.

--- tools/sonarcloud.bat
+++ tools/sonarcloud.bat
@@ -2,12 +2,12 @@
 
 SET JAVA_HOME=C:\Program Files\Java\jdk1.8.0_251
 SET ANDROID_SDK_ROOT=%USERPROFILE%\AppData\Local\Android\Sdk
-SET ANDROID_NDK_ROOT=%USERPROFILE%\AppData\Local\Android\Sdk\ndk\20.0.5594570
-SET ANDROID_NDK_HOST=windows
+SET ANDROID_NDK_ROOT=%USERPROFILE%\AppData\Local\Android\Sdk\ndk\21.0.6113669
+SET ANDROID_NDK_HOST=windows-x86_64
 SET SONARCLOUD_ROOT=D:\Users\%USERNAME%\Documents\SonarCloud
 SET ANDROID_PLATFORM=android-29
 
-SET PATH=%PATH%;C:\Qt\5.12.8\android_armv7\bin;%ANDROID_NDK_ROOT%\prebuilt\windows\bin;%SONARCLOUD_ROOT%\build-wrapper-win-x86;%SONARCLOUD_ROOT%\sonar-scanner-4.3.0.2102-windows\bin
+SET PATH=%PATH%;C:\Qt\5.12.8\android_armv7\bin;%ANDROID_NDK_ROOT%\prebuilt\windows-x86_64\bin;%SONARCLOUD_ROOT%\build-wrapper-win-x86;%SONARCLOUD_ROOT%\sonar-scanner-4.3.0.2102-windows\bin
 
 IF NOT "%~1"=="" (
         CD .. ^
